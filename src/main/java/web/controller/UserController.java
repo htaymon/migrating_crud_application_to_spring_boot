33ca3cec98a76ffba212596214ca24e3ff41e656
Htay Htay Mon <htayhtaymon@Htays-MacBook-Pro.local>
2025-12-25 18:42:58 +0900

    Migrating a CRUD Application to Spring Boot task

--- src/main/java/web/controller/UserController.java
+++ src/main/java/web/controller/UserController.java
@@ -1,6 +1,6 @@
 package web.controller;
 
-import jakarta.validation.Valid;
+import javax.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
